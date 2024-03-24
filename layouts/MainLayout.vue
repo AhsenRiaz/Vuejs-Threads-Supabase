868d5a37251dc0ab72481bd054743354eaa6a417
AhsenRiaz <ahsenriaz2001@gmail.com>
2024-03-24 22:06:54 +0500

    feat: create layout and index page

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <div class="h-screen border" id="MainLayout">
+        <div class="w-full max-w-[500px] mx-auto border" >
+            <div id="TopMenu" class="w-full flex items-center justify-center px-2 h-10 my-2" >
+                <img src="/threads-logo.png" class="w-[35px]" alt="threads-logo">
+            </div>
+        </div>
+    </div>
+</template>
